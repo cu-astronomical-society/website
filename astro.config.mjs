6e5a86ba9f367c1e987511b3353d78db1d6da936
Thomas Holland <101332229+0thomasholland@users.noreply.github.com>
2025-03-17 15:29:19 +0000

    fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://cu-astronomical-society.github.io/website/",
+    site: "https://cu-astronomical-society.github.io",
+    base: "/website",
     vite: {
         plugins: [tailwindcss()],
       },
