7207a7f33e5e563a49ebdb0fd255fcf0bd642c68
Thomas Holland <101332229+0thomasholland@users.noreply.github.com>
2025-04-28 21:29:26 +0100

    push to main website

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://beta.cuastronomy.co.uk/",
+    site: "https://cuastronomy.co.uk/",
     base: "",
     vite: {
         plugins: [tailwindcss()],
